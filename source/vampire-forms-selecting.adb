5491a81d8b1c18a054d16d26fbdd9b394018181d
ytomino <demoonlit@panathenaia.halfmoon.jp>
2016-05-02 14:01:53 +0900

    Reformat

--- source/vampire-forms-selecting.adb
+++ source/vampire-forms-selecting.adb
@@ -2,17 +2,17 @@
 with Vampire.Forms.Full;
 with Vampire.Forms.Mobile;
 package body Vampire.Forms.Selecting is
-
-function Select_Form (
-	Query_Strings : Web.Query_Strings;
-	Speeches_Per_Page : Positive)
-	return Root_Form_Type'Class is
-begin
-	if Web.Element (Query_Strings, "b") = "k" then
-		return Mobile.Form_Type'(Mobile.Create (Speeches_Per_Page));
-	else
-		return Full.Form_Type'(Full.Create);
-	end if;
-end Select_Form;
-
+	
+	function Select_Form (
+		Query_Strings : Web.Query_Strings;
+		Speeches_Per_Page : Positive)
+		return Root_Form_Type'Class is
+	begin
+		if Web.Element (Query_Strings, "b") = "k" then
+			return Mobile.Form_Type'(Mobile.Create (Speeches_Per_Page));
+		else
+			return Full.Form_Type'(Full.Create);
+		end if;
+	end Select_Form;
+	
 end Vampire.Forms.Selecting;
--- source/vampire-forms-selecting.ads
+++ source/vampire-forms-selecting.ads
@@ -1,9 +1,9 @@
 -- The Village of Vampire by YT, このソースコードはNYSLです
 package Vampire.Forms.Selecting is
-
-function Select_Form (
-	Query_Strings : Web.Query_Strings;
-	Speeches_Per_Page : Positive)
-	return Root_Form_Type'Class;
-
+	
+	function Select_Form (
+		Query_Strings : Web.Query_Strings;
+		Speeches_Per_Page : Positive)
+		return Root_Form_Type'Class;
+	
 end Vampire.Forms.Selecting;
