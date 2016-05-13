ea43e876a8ff87e53c5876ccc465d3fd4c5ac50c
ytomino <demoonlit@panathenaia.halfmoon.jp>
2016-05-13 10:00:35 +0900

    Delete the special exception of CATV for access-control, since 2006

--- source/vampire-main.adb
+++ source/vampire-main.adb
@@ -117,7 +117,6 @@ begin
 		if Post and then (Remote_Host = "" or else Remote_Host = Remote_Addr)
 			and then Remote_Addr /= "127.0.0.1" -- localhost
 			and then Remote_Addr /= "::1" -- IPv6 localhost
-			and then Remote_Addr /= "202.95.187.49" -- CATV
 		then
 			Web.Header_503 (Output);
 			Web.Header_Break (Output);
